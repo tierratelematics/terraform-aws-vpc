ef4fdc685df5610cfa105c6a80008baf82b87883
Maurizio Branca <mbranca@tierratelematics.com>
2017-08-03 07:42:53 +0200

    Add simple VPC setup

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,36 @@
+
+/**
+ * Required Variables.
+ */
+
+variable "project" {
+  description = "Name of project"
+}
+
+variable "environment" {
+  description = "Name of environment (i.e. dev, test, prod)"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+}
+
+variable "external_subnets" {
+  description = "List of external subnets"
+  type        = "list"
+}
+
+variable "internal_subnets" {
+  description = "List of internal subnets"
+  type        = "list"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = "list"
+}
+
+variable "name" {
+  description = "Name tag, e.g stack"
+  default     = "stack"
+}
